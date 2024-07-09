dd689fd7ec4e4b823038e4a9035edeff71859e88
Kyler Long <kylerlong24@gmail.com>
2024-07-08 23:11:07 -0400

    add logic for for reverseString function logic converts string to array, reverses the characters, and joins them back together into a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,12 @@
-const reverseString = function(greeting) {
-    let greetingArray = [greeting];
-    greetingArray.
+//creat a variable to store the string parameter as an array
+//split method with empty string for selector will create an array of characters
+//reverse the characters in the array using reverse method
+//convert the array back to a string and return output with join method using empty string for selector again
 
+const reverseString = function(string) {
+    let array = string.split("");
+    array.reverse();
+    return array.join("");
 };
 
 // Do not edit below this line
