919c21908ff578e002eba277d82324a2d3bcbac6
Kyler Long <kylerlong24@gmail.com>
2024-07-14 19:06:26 -0400

    create for of loop to iterate through removed items, assigning the value of removed to current variable Create an inner for loop that that iterates through the array with an inner if statement to splice out the index when it is equal to the removed from the for of loop as long as the typeof is the same.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...removedItems) {
+    
+    for (let removed of removedItems) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === removed && typeof(array[i]) === typeof(removed)) {
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    return array;
+
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
