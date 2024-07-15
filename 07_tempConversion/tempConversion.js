90e9a2e35c1294be5f3e5a02caf98684bb724ab3
Kyler Long <kylerlong24@gmail.com>
2024-07-14 21:47:06 -0400

    use parseFloat and toFixed(1) to round temp conversions to one decimal place and convert to a number.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  const celsiusResult = (fTemp - 32) * (5/9);
+  return parseFloat(celsiusResult.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  const farenheitResult = (cTemp * 9/5) + 32;
+  return parseFloat(farenheitResult.toFixed(1));
 };
 
 // Do not edit below this line
