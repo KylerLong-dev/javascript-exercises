7d922b767bfe6289b8e378dbf9c717cedbcd6ad2
Kyler Long <kylerlong24@gmail.com>
2024-08-13 23:04:24 -0400

    Complete calculator js exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return sumOfArray = array.reduce((total, currentNumber) => total + currentNumber, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return multiplyArray = array.reduce((total, currentNumber) => total * currentNumber, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  const array = [];
+  for (let i = 1; i<=num; i++){
+    array.push(i);
+  }
+  return array.reduce((total, currentNum) => {
+    return total * currentNum;
+  }, 1);
 };
 
 // Do not edit below this line
